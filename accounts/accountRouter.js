a200c980ae158d82b990547b3014a7533eef4b5d
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-11-04 15:16:05 +0100

    created an endpoint to getby id

--- accounts/accountRouter.js
+++ accounts/accountRouter.js
@@ -6,12 +6,23 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     db('accounts')
-    .then(account => {
-        res.status(200).json(account);
-    })
-    .catch(error => {
-        res.status(500).json({ message: 'this did not go right' + error})
-    })
+        .then(account => {
+            res.status(200).json(account);
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'this did not go right' + error })
+        })
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const account = await db('accounts').where({ id: req.params.id });
+        res.json(account[0]);
+    }
+    catch (error) {
+        res.status(500).json({ message: 'this did not go right' + error.message })
+    }
+
 });
 
 module.exports = router

1a2e862de84ed4a490d114a1661046c8dd7efc7a
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-11-04 15:04:14 +0100

    updated get endpoint

--- accounts/accountRouter.js
+++ accounts/accountRouter.js
@@ -7,10 +7,11 @@ const router = express.Router();
 router.get('/', (req, res) => {
     db('accounts')
     .then(account => {
-        res.json(200).json(account);
+        res.status(200).json(account);
     })
     .catch(error => {
         res.status(500).json({ message: 'this did not go right' + error})
     })
 });
 
+module.exports = router

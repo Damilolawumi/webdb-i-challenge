b45cb14bf7e92a40b2d49ce517fed967dd78f562
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-11-04 14:44:41 +0100

    created an endpoint for get

--- accounts/accountRouter.js
+++ accounts/accountRouter.js
@@ -4,3 +4,13 @@ const db = require('../data/dbConfig');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    db('accounts')
+    .then(account => {
+        res.json(200).json(account);
+    })
+    .catch(error => {
+        res.status(500).json({ message: 'this did not go right' + error})
+    })
+});
+
